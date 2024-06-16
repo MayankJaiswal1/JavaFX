bcfed8c845a37209f45ae9b97771fc610e70465d
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-06-16 22:24:26 +0530

    JavaFX

---
+++ DailyWork/Day1/src/App.java
@@ -0,0 +1,7 @@
+import javafx.application.Application;
+
+public class App extends Application{
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
