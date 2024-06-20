21c4ac0527bfe5c28f1366e3ef7668fbc06dfc4a
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-06-20 18:36:53 +0530

    FX-A3

--- Practical/Assignment-3/src/buttonPAge/ColorfulWindow.java
+++ Practical/Assignment-3/src/buttonPAge/ColorfulWindow.java
@@ -1,15 +1,13 @@
 package buttonPAge;
 
-import java.util.Random;
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class ColorfulWindow extends Application {
@@ -29,13 +27,30 @@ public class ColorfulWindow extends Application {
         root.setAlignment(Pos.CENTER);
         root.getChildren().add(colorButton);
 
-        Scene scene = new Scene(root, 500, 500);
+        HBox hbclick = new HBox(colorButton);
 
+        Scene scene = new Scene(hbclick, 500, 500);
+
+        int[] clickcount = new int[]{0};
         colorButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                Color randomColor = getRandomColor();
-                root.setStyle("-fx-background-color: " + toRgbString(randomColor) + ";");
+                if(clickcount[0]%5==0){
+                    hbclick.setStyle("-fx-background-color: Gold");
+                }if(clickcount[0]%5==1){
+                    hbclick.setStyle("-fx-background-color: Red");
+                }if(clickcount[0]%5==2){
+                    hbclick.setStyle("-fx-background-color: Green");
+                }if(clickcount[0]%5==3){
+                    hbclick.setStyle("-fx-background-color: SandBrown");
+                }if(clickcount[0]%5==4){
+                    hbclick.setStyle("-fx-background-color: Blue");
+                }
+                clickcount[0]++;
+
+
+                // Color randomColor = getRandomColor();
+                // root.setStyle("-fx-background-color: " + toRgbString(randomColor) + ";");
             }
         });
 
@@ -43,16 +58,16 @@ public class ColorfulWindow extends Application {
         primaryStage.show();
     }
 
-    private Color getRandomColor() {
-        Random random = new Random();
-        return Color.rgb(random.nextInt(256), random.nextInt(256), random.nextInt(256));
-    }
+    // private Color getRandomColor() {
+    //     Random random = new Random();
+    //     return Color.rgb(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+    // }
 
-    private String toRgbString(Color color) {
-        int red = (int) (color.getRed() * 255);
-        int green = (int) (color.getGreen() * 255);
-        int blue = (int) (color.getBlue() * 255);
-        return String.format("rgb(%d, %d, %d)", red, green, blue);
-    }
+    // private String toRgbString(Color color) {
+    //     int red = (int) (color.getRed() * 255);
+    //     int green = (int) (color.getGreen() * 255);
+    //     int blue = (int) (color.getBlue() * 255);
+    //     return String.format("rgb(%d, %d, %d)", red, green, blue);
+    // }
 }
 
