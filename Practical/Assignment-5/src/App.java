a461b1b5d45cac06e7c9d11d47368fe634350942
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-06-26 09:37:59 +0530

    FX-A5

---
+++ Practical/Assignment-5/src/App.java
@@ -0,0 +1,9 @@
+import controller.FormNavigationApp;
+import javafx.application.Application;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Application.launch(FormNavigationApp.class,args);
+    }
+}
